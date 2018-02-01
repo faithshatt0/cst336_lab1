2397184cbaea01e9d645f295899f2cb0fc88015a
Faith Shatto <fshatto@csumb.edu>
2018-02-01 01:27:32 +0000

    Add all lab 1 files

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+Personal Portfolio
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title>Faith Shatto</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet" type="text/css"</styles>
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1>Faith Shatto</h1>
+        </header>
+        
+        <nav>
+            <hr width="50%" />
+            <a href="index.php">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <main>
+            <figure id="me">
+                <img src="img/prof.JPG">
+            </figure>
+            <div id="welcomeText">
+                <br />
+                Hello! <br />
+                <p>
+                    Thank you for visiting my professional portfolio website.
+                </p>
+                <p>
+                    I am a Software Engineer currently attending CSUMB.
+                </p>
+                <p>
+                    Feel free to contact me.
+                </p>
+                
+                <br/><br/>
+                
+                <em>"With ordinary talent and extraordinary <strong>perserverance</strong>, all things are attainable."</em>
+                - Thomas F. Buxton
+            </div>
+        </main>
+        
+        <footer>
+            <hr>
+            Internet Programming. 2017
+            <br/>
+            <img src= "https://csumb.edu/sites/default/files/styles/profile_large/public/photo_1_6.jpg?itok=N4I4g9Wl">
+        </footer>
+        <!-- closing footer -->
+       
+    </body>
+    <!-- closing body -->
+
+</html>
